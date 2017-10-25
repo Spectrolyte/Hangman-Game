74a8ff0d881408c89c300a10766748a224a80166
Diana <dianaltecson@gmail.com>
2017-10-25 08:54:19 -0700

    added pseudocode to js file

---
+++ assets/javascript/game.js
@@ -0,0 +1,21 @@
+// press any key to get started
+// variable declaration
+	// wins
+	// guesses remaining
+	// losses
+	// CPU word
+	// placeholder for word '- - - -' (changes w/ correct guess)
+	// word bank
+	// letters guessed
+// when any key is pressed (outer function)
+	// assign CPUword
+	// when letter is pressed (inner function)
+		// if no guesses remaining
+				// assign new value to CPUword
+		// else if letter DNE in guessedLetters array AND there are guesses remaining
+			// store letter in array
+			// if letter matches letter(s) in CPUword
+				// replace dashes with letters
+			// else if letter doesn't match
+				// decrement guesses remaining
+
